71f2302e514e571af517fc730f6ad631b3af384f
shaniltest <shanilmm@hotmail.com>
2024-09-26 00:14:36 -0700

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,7 +6,8 @@ namespace webapp_sam_lab13.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-
+    <h1>Portfolio Gallery</h1>
+     <P>This is a simple .NET web project.</P>
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
